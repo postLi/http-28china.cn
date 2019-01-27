a4e3bb38de1633e226b90d5d29a90f4a267157c1
何永青 <864887652@qq.com>
2019-01-27 17:28:24 +0800

    资讯栏目

---
+++ pages/zixun/tlzx/_id.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-html="content" />
+</template>
+
+<script>
+export default {
+  name: 'CheYuan',
+  head: {
+    link: [{ rel: 'stylesheet', href: '/css/jquery.pagination.css' }],
+    script: [
+      { src: '/js/city-picker.data.js' },
+      { src: '/js/city-picker.js' },
+      { src: '/js/jquery.pagination.min.js' }
+    ]
+  },
+  data() {
+    return {
+      recommendList: [],
+      dataset: [],
+      content: ''
+    }
+  },
+  async asyncData({ $axios, app, query, params, route }) {
+    console.log('route.path:', route.path)
+    let con = await $axios.get(
+      'http://192.168.1.127:8080/anfacms/' + route.path.replace('/zixun', '')
+    )
+    return {
+      content: con.data
+    }
+  },
+  mounted() {},
+  methods: {
+    //
+  }
+}
+</script>

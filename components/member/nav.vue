4528b536be1624c40e0bcab9ac94204923894e3a
何永青 <864887652@qq.com>
2019-03-11 13:59:54 +0800

    公司官网页面调整

--- components/member/nav.vue
+++ components/member/nav.vue
@@ -4,45 +4,45 @@
       <ul class="nav">
         <li><a 
           id="menu_0" 
-          :href="$store.state.member.id + ''" 
+          :href="mid + ''" 
           :class="/\/[^-]*$/.test(tpath) ? 'nav-current' : ''">首页</a></li>
 
         <li><a 
           id="menu_1" 
-          :href="$store.state.member.id + '-jianjie'"
+          :href="mid + '-jianjie'"
           :class="tpath.indexOf('jianjie') !== -1 ? 'nav-current' : ''" >公司简介</a></li>
         <li><a 
           id="menu_2" 
-          :href="$store.state.member.id + '-cpfw'" 
+          :href="mid + '-cpfw'" 
           :class="tpath.indexOf('cpfw') !== -1 ? 'nav-current' : ''">产品服务 </a></li>
         <li><a 
           id="menu_3" 
-          :href="$store.state.member.id + '-wangdian'" 
+          :href="mid + '-wangdian'" 
           :class="tpath.indexOf('wangdian') !== -1 ? 'nav-current' : ''">网点分布</a></li>
             
         <li><a 
           id="menu_4" 
-          :href="$store.state.member.id + '-line'" 
+          :href="mid + '-line'" 
           :class="tpath.indexOf('line') !== -1 ? 'nav-current' : ''">物流专线</a></li>
             
         <li ><a 
           id="menu_huo" 
-          :href="$store.state.member.id + '-huo'" 
+          :href="mid + '-huo'" 
           :class="tpath.indexOf('huo') !== -1 ? 'nav-current' : ''">货源信息</a></li>
         <li ><a 
           id="menu_online_order" 
-          :href="$store.state.member.id + '-order?uid=' + $store.state.member.id + '&publishId=' + $store.state.member.id" 
+          :href="mid + '-order?uid=' + mid + '&publishId=' + mid" 
           :class="tpath.indexOf('order') !== -1 ? 'nav-current' : ''" 
           target="_blank"
           class="need_companyId">在线下单</a></li>
         <li><a 
           id="menu_5" 
-          :href="$store.state.member.id + '-webnotice'" 
+          :href="mid + '-webnotice'" 
           :class="tpath.indexOf('webnotice') !== -1 ? 'nav-current' : ''">公司动态</a></li>
             
         <li><a 
           id="menu_6" 
-          :href="$store.state.member.id + '-contactus'" 
+          :href="mid + '-contactus'" 
           :class="tpath.indexOf('contactus') !== -1 ? 'nav-current' : ''">联系我们</a></li>
       </ul>
     </div>
@@ -57,6 +57,11 @@ export default {
   data() {
     return {
       tpath: ''
+    }
+  },
+  computed: {
+    mid() {
+      return this.$store.state.member.id
     }
   },
   mounted() {
--- components/member/sidebar2.vue
+++ components/member/sidebar2.vue
@@ -3,15 +3,15 @@
     <ul><li class="side_title"><span>客户服务</span></li>
       <li 
         :class="tpath.indexOf('order') !== -1 ? 'active' : ''" 
-        class="side_memu"><a :href="$store.state.member.id + '-order'"><span>在线下单</span></a></li>
+        class="side_memu"><a :href="mid + '-order'"><span>在线下单</span></a></li>
       <li 
         :class="tpath.indexOf('order') === -1 ? 'active' : ''" 
         class="side_memu"><span>查询服务</span></li>
       <ul class="side_submemu ">
-        <li :class="tpath.indexOf('chajian') !== -1 ? 'now' : ''"><a :href="$store.state.member.id + '-chajian'"><span>查件（物流追踪）</span></a></li>
-        <li :class="tpath.indexOf('line') !== -1 ? 'now' : ''"><a :href="$store.state.member.id + '-line'"><span>物流专线</span></a></li>
-        <li :class="tpath.indexOf('wangdian') !== -1 ? 'now' : ''"><a :href="$store.state.member.id + '-wangdian'"><span>网点分布</span></a></li>
-        <li :class="tpath.indexOf('huo') !== -1 ? 'now' : ''"><a :href="$store.state.member.id + '-huo'"><span>货源信息</span></a></li>
+        <li :class="tpath.indexOf('chajian') !== -1 ? 'now' : ''"><a :href="mid + '-chajian'"><span>查件（物流追踪）</span></a></li>
+        <li :class="tpath.indexOf('line') !== -1 ? 'now' : ''"><a :href="mid + '-line'"><span>物流专线</span></a></li>
+        <li :class="tpath.indexOf('wangdian') !== -1 ? 'now' : ''"><a :href="mid + '-wangdian'"><span>网点分布</span></a></li>
+        <li :class="tpath.indexOf('huo') !== -1 ? 'now' : ''"><a :href="mid + '-huo'"><span>货源信息</span></a></li>
       </ul>
     </ul>	
   </div>
@@ -21,6 +21,11 @@ export default {
   data() {
     return {
       tpath: ''
+    }
+  },
+  computed: {
+    mid() {
+      return this.$store.state.member.id
     }
   },
   mounted() {

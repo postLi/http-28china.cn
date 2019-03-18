3b57561d569c99fcfac61442633538c2034e1dc6
何永青 <864887652@qq.com>
2019-03-18 10:16:41 +0800

    修正帮助中心样式

--- pages/help/utils.js
+++ pages/help/utils.js
@@ -4,6 +4,7 @@ export function formatAnfaTms(str = '') {
     .replace(/href=("|')http:\/\/192\.168\.1\.79\/anfacms/gim, 'href=$1/help')
     .replace(/src="\/zixun\//gim, 'src="/anfacms/')
     .replace(/href="\/anfacms/gim, 'href="/help')
+    .replace(/http:\/\/192.168.1.79\/anfacms/gim, '/help')
 }
 
 export function makeAnfaTmsUrl(path) {

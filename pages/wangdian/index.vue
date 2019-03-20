3daf3b76223ca8f7f9ae6c31a465a424ead37017
lll <1076408188@qq.com>
2019-03-20 15:54:58 +0800

    网点搜索

--- pages/wangdian/index.vue
+++ pages/wangdian/index.vue
@@ -161,7 +161,9 @@
                   <div
                     id="js010"
                     class="wlzx_yq_nr">
-                    <div class="wlzx_yq_none">
+                    <div 
+                      v-if="logisticsPark.length==0"
+                      class="wlzx_yq_none">
                       <font>暂无园区信息</font>
                     </div>
 
@@ -525,6 +527,7 @@ export default {
     }
   },
   mounted() {
+    this.companyName = this.$route.query.companyName || ''
     seajs.use(['/js/gaodemap2.js'])
     $('.collapse').click(function() {
       $('.collapse').css('display', 'none')
@@ -666,6 +669,7 @@ export default {
         locationProvince: this.vo.startProvince,
         ...this.vo
       })
+      console.log(this.logisticsPark, 'logisticsPark')
     },
     pagination() {
       console.log('this.pages:', this.pages)

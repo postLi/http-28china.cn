e1e3d8214b1ad95396ee0202235d3195e28da5f7
何永青 <864887652@qq.com>
2019-04-10 12:37:21 +0800

    修改请求超时时间 调整服务器端请求代码

--- plugins/axios.js
+++ plugins/axios.js
@@ -50,8 +50,8 @@ $axios :
   } */
 
   // 基本配置
-  // axios.defaults.timeout = 30000
-  axios.defaults.timeout = 5000
+  axios.defaults.timeout = 30000
+  // axios.defaults.timeout = 5000
   axios.defaults.baseUrl = ''
   // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 
--- server/index.js
+++ server/index.js
@@ -13,26 +13,31 @@ const port = process.env.PORT || 3000
 async function start() {
   // Instantiate nuxt.js
   const nuxt = new Nuxt(config)
-  // 版本更新到2.5后需执行这条语句
-  await nuxt.ready()
 
   // Build in development
   if (config.dev) {
     const builder = new Builder(nuxt)
     await builder.build()
+  } else {
+    // 版本更新到2.5后需执行这条语句
+    await nuxt.ready()
   }
 
   app.use(ctx => {
     ctx.status = 200 // koa defaults to 404 when it sees that status is unset
 
-    return new Promise((resolve, reject) => {
+    ctx.respond = false // Bypass Koa's built-in response handling
+    ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
+    nuxt.render(ctx.req, ctx.res)
+
+    /* return new Promise((resolve, reject) => {
       ctx.res.on('close', resolve)
       ctx.res.on('finish', resolve)
       nuxt.render(ctx.req, ctx.res, promise => {
         // nuxt.render passes a rejected promise into callback on error.
         promise.then(resolve).catch(reject)
       })
-    })
+    }) */
   })
 
   app.listen(port, host)
